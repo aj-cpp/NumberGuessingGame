5863e02d89d6794a1478cb4365b39f655ab322ed
aj-cpp <ajujun@cpp.edu>
2022-10-22 13:21:45 -0700

    Initial commit, Add outline

---
+++ main.c
@@ -0,0 +1,14 @@
+// CS2600 - Andrew Jujun
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // Outline:
+  // 1. Create necessary functions -> randomize, show menu, play game, set max
+  // 2. Create command handler
+  // 3. Set user constraints
+  // 4. Create main
+  
+  return EXIT_SUCCESS;
+}
